8a40a38f875cb574752f35e185c7bf6e06dc279e
hikarimusic2002 <hikarimusic2002@gmail.com>
2022-02-06 14:25:27 +0800

    Create 0001B_Spreadsheets.cpp

---
+++ 0001B_Spreadsheets.cpp
@@ -0,0 +1,45 @@
+//Codeforces 1B - Spreadsheets
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n ;
+string str ;
+
+int main()
+{
+    cin>>n ;
+    for (int t=0;t<n;++t) {
+        cin>>str ;
+        int k=1 ;
+        int len=str.length() ;
+        while (k<len && isdigit(str[k]) ) {
+            ++k ;
+        }
+        if (k==1 || k==len) {
+            int m=0, col=0 ;
+            while (isalpha(str[m]) ) {
+                col=col*26+str[m]-'A'+1 ;
+                ++m ;
+            }
+            string ans='R'+str.substr(m,len-m) ;
+            ans+='C'+to_string(col) ;
+            cout<<ans<<'\n' ;
+        }
+        else {
+            int row=0 ;
+            for (int i=k+1;i<len;++i) {
+                row=row*10+str[i]-'0' ;
+            }
+            string temp ;
+            while (row>0) {
+                temp+=(row-1)%26+'A' ;
+                row=(row-1)/26 ;
+            }
+            reverse(temp.begin(),temp.end()) ;
+            string ans=temp+str.substr(1,k-1) ;
+            cout<<ans<<'\n' ;
+        }
+    }
+    return 0;
+}
