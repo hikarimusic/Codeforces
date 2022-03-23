24d4b0dc024d4c37826dc608a9d7b6bbb3d7b813
hikarimusic2002 <hikarimusic2002@gmail.com>
2022-03-23 23:22:37 +0800

    Create 0131A_cAPS_lOCK.cpp

---
+++ 0131A_cAPS_lOCK.cpp
@@ -0,0 +1,25 @@
+//Codeforces 131A - cAPS lOCK 
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+string s ;
+
+int main() {
+    cin>>s ;
+    bool b=1 ;
+    for (int i=1;i<s.length();++i) {
+        if (s[i]>=97) {
+            b=0 ;
+            break ;
+        }
+    }
+    if (b) {
+        s[0]+=(s[0]<97)?(32):(-32) ;
+        for (int i=1;i<s.length();++i) {
+            s[i]+=32 ;
+        }
+    }
+    cout<<s ;
+    return 0 ;
+}
