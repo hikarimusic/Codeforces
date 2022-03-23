4c8a09eb2954a4ca7102996c190ade926dbec736
hikarimusic2002 <hikarimusic2002@gmail.com>
2022-03-23 18:24:24 +0800

    Create 0118A_String_Task.cpp

---
+++ 0118A_String_Task.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+string s ;
+
+int main() {
+    cin>>s ;
+    for (int i=0;i<s.length();++i) {
+        char c=s[i] ;
+        if (c<97) c+=32 ;
+        if (c=='a'||c=='o'||c=='y'||c=='e'||c=='u'||c=='i') continue ;
+        cout<<'.'<<c ;
+    }
+    return 0 ;
+}
