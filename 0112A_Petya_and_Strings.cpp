269a7f0b8147327dcf8441c1399a0114b3eb2516
hikarimusic2002 <hikarimusic2002@gmail.com>
2022-03-23 18:12:59 +0800

    Create 0112A_Petya_and_Strings.cpp

---
+++ 0112A_Petya_and_Strings.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+string a,b ;
+
+int main() {
+    cin>>a>>b ;
+    int t=0 ;
+    for (int i=0;i<a.length();++i) {
+        if (a[i]<97) a[i]+=32 ;
+        if (b[i]<97) b[i]+=32 ;
+        if (a[i]<b[i]) {
+            t=-1 ;
+            break ;
+        }
+        if (a[i]>b[i]) {
+            t=1 ;
+            break ;
+        }
+    }
+    cout<<t ;
+    return 0 ;
+}
