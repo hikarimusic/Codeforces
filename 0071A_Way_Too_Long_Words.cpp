3d6bb78566c05a27d7119d620049fe16198eb63d
hikarimusic2002 <hikarimusic2002@gmail.com>
2022-03-23 18:00:23 +0800

    Create 0071A_Way_Too_Long_Words.cpp

---
+++ 0071A_Way_Too_Long_Words.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int n ;
+string s ;
+
+int main() {
+    cin>>n ;
+    while (n--) {
+        cin>>s ;
+        int l=s.length() ;
+        if (l<=10) cout<<s<<'\n' ;
+        else cout<<s[0]<<l-2<<s[l-1]<<'\n' ;
+    }
+    return 0 ;
+}
