c2d788ce7617158d2d73496e7ba687856fb7734a
hikarimusic2002 <hikarimusic2002@gmail.com>
2022-03-23 22:49:30 +0800

    Create 0281A_Word_Capitalization.cpp

---
+++ 0281A_Word_Capitalization.cpp
@@ -0,0 +1,13 @@
+//Codeforces 281A - Word Capitalization
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+string s ;
+
+int main() {
+    cin>>s ;
+    if (s[0]>=97) s[0]-=32 ;
+    cout<<s ;
+    return 0 ;
+}
