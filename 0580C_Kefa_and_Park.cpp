51876f7aa10a1006b4fae7ecf00c70676b689992
hikarimusic2002 <hikarimusic2002@gmail.com>
2022-03-25 00:16:53 +0800

    Create 0580C_Kefa_and_Park.cpp

---
+++ 0580C_Kefa_and_Park.cpp
@@ -0,0 +1,34 @@
+//Codeforces 580C - Kefa and Park
+
+#include <bits/stdc++.h>
+using namespace std ;
+#define MAXN 100005
+
+int n,m,ans ;
+vector<vector<int>> g(MAXN) ;
+vector<int> c(MAXN) ;
+
+void dfs (int u, int v, int s) {
+    int ss=(c[u]==1)?(s+1):0 ;
+    if (ss>m) return ;
+    for (int w : g[u]) {
+        if (w!=v) dfs(w,u,ss) ;
+    }
+    if (u!=1 && g[u].size()==1) ans++ ;
+}
+
+int main() {
+    scanf("%d %d",&n,&m) ;
+    for (int i=1;i<=n;++i) {
+        scanf("%d",&c[i]) ;
+    }
+    for (int i=0;i<n-1;++i) {
+        int a, b;
+        scanf("%d %d",&a,&b) ;
+        g[a].push_back(b) ;
+        g[b].push_back(a) ;
+    }
+    dfs(1,0,0) ;
+    printf("%d",ans) ;
+    return 0 ;
+}
