1896ab11749776c62330d284706746af36ba765a
hikarimusic2002 <hikarimusic2002@gmail.com>
2022-03-24 08:39:46 +0800

    Create 0500A_New_Year_Transportation.cpp

---
+++ 0500A_New_Year_Transportation.cpp
@@ -0,0 +1,17 @@
+//Codeforces 500A - New Year Transportation
+
+#include <bits/stdc++.h>
+using namespace std ;
+#define MAXN 30005
+
+int n,t ;
+int a[MAXN] ;
+
+int main() {
+    scanf("%d %d",&n,&t) ;
+    for (int i=1;i<=n-1;++i) scanf("%d",&a[i]) ;
+    int c=1 ;
+    while (c<t) c+=a[c] ;
+    printf("%s",(c==t)?"YES":"NO") ;
+    return 0 ;
+}
