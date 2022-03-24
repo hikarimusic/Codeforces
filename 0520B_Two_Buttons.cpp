19cd3d4a8a6e3162cd3f2a41bada2056cd09556e
hikarimusic2002 <hikarimusic2002@gmail.com>
2022-03-24 15:01:10 +0800

    Create 0520B_Two_Buttons.cpp

---
+++ 0520B_Two_Buttons.cpp
@@ -0,0 +1,22 @@
+//Codeforces 520B - Two Buttons
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+int n,m ;
+
+int main() {
+    scanf("%d %d",&n,&m) ;
+    int c=0 ;
+    while (m>n) {
+        if (m&1) {
+            m+=1 ;
+            c++ ;
+        }
+        m/=2 ;
+        c++ ;
+    }
+    c+=(n-m) ;
+    printf("%d",c) ;
+    return 0 ;
+}
